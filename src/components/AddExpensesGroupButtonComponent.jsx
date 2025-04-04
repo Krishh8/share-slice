a1c7e646824159f6b0728b51967a1ec775737302
KrishKheni <khenikrish08@gmail.com>
2025-04-04 15:08:31 +0530

    button position

--- src/components/AddExpensesGroupButtonComponent.jsx
+++ src/components/AddExpensesGroupButtonComponent.jsx
@@ -3,7 +3,11 @@ import React, { useState } from 'react';
 import { FAB, Portal, useTheme } from 'react-native-paper';
 import { useIsFocused, useNavigation } from '@react-navigation/native';
 import AddMemberModal from './AddMemberModal';
-import { responsiveFontSize as rfs } from 'react-native-responsive-dimensions';
+import {
+    responsiveFontSize as rfs,
+    responsiveHeight as rh,
+    responsiveWidth as rw,
+} from 'react-native-responsive-dimensions';
 
 const AddExpensesGroupButtonComponent = ({ open, onStateChange }) => {
     const theme = useTheme();
@@ -33,7 +37,8 @@ const AddExpensesGroupButtonComponent = ({ open, onStateChange }) => {
                             backdrop: "rgba(0, 0, 0, 0.7)", // Adjust opacity here (0.3 for lighter effect)
                         },
                     }}
-                    fabStyle={{ backgroundColor: theme.colors.primary }}
+                    style
+                    fabStyle={{ backgroundColor: theme.colors.primary, position: 'absolute', bottom: rh(4), right: rh(2) }}
                     actions={[
                         {
                             icon: 'account-plus',
@@ -41,6 +46,8 @@ const AddExpensesGroupButtonComponent = ({ open, onStateChange }) => {
                             labelTextColor: theme.colors.secondary,
                             color: theme.colors.primary,
                             onPress: handleAddMember,
+                            style: { position: 'absolute', bottom: rh(15), right: rh(2) },
+                            labelStyle: { position: 'absolute', bottom: rh(15), right: rh(5) },
                         },
                         {
                             icon: 'notebook-plus',
@@ -51,6 +58,8 @@ const AddExpensesGroupButtonComponent = ({ open, onStateChange }) => {
                                 onStateChange({ open: false }); // Close FAB
                                 navigation.navigate('AddExpense');
                             },
+                            style: { position: 'absolute', bottom: rh(13), right: rh(2) },
+                            labelStyle: { position: 'absolute', bottom: rh(13), right: rh(5) },
                         },
                     ]}
                     onStateChange={onStateChange}
